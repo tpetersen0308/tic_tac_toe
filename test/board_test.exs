e47459c13f93cf06be7eb128cfde919cf5afb130
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-08 15:33:09 -0700

    add unit test to check that Board.is_full returns true for a full board

--- test/board_test.exs
+++ test/board_test.exs
@@ -5,4 +5,8 @@ defmodule BoardTest do
   test "it can create an empty board" do 
     assert %{ 1 => nil, 2 => nil, 3 => nil, 4 => nil, 5 => nil, 6 => nil, 7 => nil, 8 => nil, 9 => nil } == Board.empty
   end
+
+  test "it can check for a full board" do
+    assert Board.is_full(%{ 1 => "X", 2 => "O", 3 => "X", 4 => "O", 5 => "X", 6 => "O", 7 => "X", 8 => "O", 9 => "X" })
+  end
 end
