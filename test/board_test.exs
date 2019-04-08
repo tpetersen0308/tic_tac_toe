fe5bf98d97ba239f2376971e2a757390e01a23b0
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-08 15:36:10 -0700

    add another unit test for Board.is_full to check if it can return false when a position is available

--- test/board_test.exs
+++ test/board_test.exs
@@ -6,7 +6,13 @@ defmodule BoardTest do
     assert %{ 1 => nil, 2 => nil, 3 => nil, 4 => nil, 5 => nil, 6 => nil, 7 => nil, 8 => nil, 9 => nil } == Board.empty
   end
 
-  test "it can check for a full board" do
-    assert Board.is_full(%{ 1 => "X", 2 => "O", 3 => "X", 4 => "O", 5 => "X", 6 => "O", 7 => "X", 8 => "O", 9 => "X" })
+  describe "Board.is_full" do
+    test "it returns true for a full board" do
+      assert Board.is_full(%{ 1 => "X", 2 => "O", 3 => "X", 4 => "O", 5 => "X", 6 => "O", 7 => "X", 8 => "O", 9 => "X" })
+    end
+
+    test "it returns false for a board that has at least one available position" do
+      assert !Board.is_full(%{ 1 => "X", 2 => "O", 3 => "X", 4 => "O", 5 => "X", 6 => nil, 7 => "X", 8 => "O", 9 => "X" })
+    end
   end
 end
