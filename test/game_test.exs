f026a5a34a46c27897ed31a6f3e0050ade3121c3
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-08 16:26:59 -0700

    refactor Board.current_player unit tests to test for cases of returning both X and O

--- test/game_test.exs
+++ test/game_test.exs
@@ -9,10 +9,19 @@ defmodule GameTest do
     assert 3 == Game.turn_count(board)
   end
 
-  test "it can return the current player's token" do
-    board = Board.empty
-    board = Board.update(board, 5, "X") |> Board.update(8, "O") |> Board.update(2, "X")
+  describe "Board.current_player" do
+    @board Board.empty
+    @board Board.update(@board, 5, "X") |> Board.update(8, "O") |> Board.update(2, "X")
+    
+    test "it returns 'O' when the turn count is odd" do
+      assert "O" == Game.current_player(@board)
+    end
 
-    assert "O" == Game.current_player(board)
+    @board Board.update(@board, 6, "O")
+    
+    test "it returns 'X' when the turn count is even" do
+
+      assert "X" == Game.current_player(@board)
+    end
   end
 end
