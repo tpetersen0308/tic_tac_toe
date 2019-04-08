14404613f11323bdf0c306bf805297af4a66c072
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-08 16:20:07 -0700

    add function to return current players token

--- lib/game.ex
+++ lib/game.ex
@@ -2,4 +2,8 @@ defmodule Game do
   def turn_count(board) do
     Enum.filter(Map.values(board), &(&1)) |> Enum.count
   end
+
+  def current_player(board) do
+    if Integer.mod(turn_count(board), 2) == 0, do: "X", else: "O"
+  end
 end
