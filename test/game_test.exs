9ba818d30de57128d47f8030880029219f61a5e7
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-08 16:19:55 -0700

    add unit test to check if Game.current_player can return the current players token

--- test/game_test.exs
+++ test/game_test.exs
@@ -8,4 +8,11 @@ defmodule GameTest do
 
     assert 3 == Game.turn_count(board)
   end
+
+  test "it can return the current player's token" do
+    board = Board.empty
+    board = Board.update(board, 5, "X") |> Board.update(8, "O") |> Board.update(2, "X")
+
+    assert "O" == Game.current_player(board)
+  end
 end
