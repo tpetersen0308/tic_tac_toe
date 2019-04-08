0a2747bad080ceddd954248806b52f20d7261429
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-08 15:33:22 -0700

    add function to check if board is full

--- lib/board.ex
+++ lib/board.ex
@@ -2,4 +2,8 @@ defmodule Board do
   def empty do
     %{ 1 => nil, 2 => nil, 3 => nil, 4 => nil, 5 => nil, 6 => nil, 7 => nil, 8 => nil, 9 => nil }
   end
+
+  def is_full(board) do
+    Enum.all?(Map.values(board), &(&1))
+  end
 end
