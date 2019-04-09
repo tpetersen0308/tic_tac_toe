d6e49a585545e3e7c03b801eca68a82eeaedd96e
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-09 09:38:27 -0700

    refactor Game.check_win to eliminate duplication

--- lib/game.ex
+++ lib/game.ex
@@ -8,7 +8,12 @@ defmodule Game do
   end
 
   def check_win(board) do
-    board[1] == board[2] and board[1] == board[3] and !!board[1] or
-    board[4] == board[5] and board[4] == board[6] and board[4]
+    [{1,2,3}, {4,5,6}] |> Enum.map(fn combo ->
+      {pos1, pos2, pos3} = {elem(combo, 0), elem(combo, 1), elem(combo, 2)}
+      board[pos1] == board[pos2] and board[pos1] == board[pos3] and !!board[pos1]
+    end) 
+      |> Enum.any?(&(&1))
+    # board[1] == board[2] and board[1] == board[3] and !!board[1] or
+    # board[4] == board[5] and board[4] == board[6] and board[4]
   end
 end
