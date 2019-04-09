745bb0b4876e92aa7cb4f1cb9071d590e99126b8
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-09 09:39:48 -0700

    extract win combinations into attribute variable

--- lib/game.ex
+++ lib/game.ex
@@ -1,4 +1,6 @@
 defmodule Game do
+  @win_combos [{1,2,3}, {4,5,6}]
+  
   def turn_count(board) do
     Enum.filter(Map.values(board), &(&1)) |> Enum.count
   end
@@ -8,12 +10,10 @@ defmodule Game do
   end
 
   def check_win(board) do
-    [{1,2,3}, {4,5,6}] |> Enum.map(fn combo ->
+    @win_combos |> Enum.map(fn combo ->
       {pos1, pos2, pos3} = {elem(combo, 0), elem(combo, 1), elem(combo, 2)}
       board[pos1] == board[pos2] and board[pos1] == board[pos3] and !!board[pos1]
     end) 
       |> Enum.any?(&(&1))
-    # board[1] == board[2] and board[1] == board[3] and !!board[1] or
-    # board[4] == board[5] and board[4] == board[6] and board[4]
   end
 end
