0383c287807ec809a0951f1c05128d42577b5260
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-08 17:27:42 -0700

    add function to check if there is a winning combination on the board

--- lib/game.ex
+++ lib/game.ex
@@ -6,4 +6,9 @@ defmodule Game do
   def current_player(board) do
     if Integer.mod(turn_count(board), 2) == 0, do: "X", else: "O"
   end
+
+  def check_win(board) do
+    board[1] == board[2] and board[1] == board[3] and !!board[1] or
+    board[4] == board[5] and board[4] == board[6] and board[4]
+  end
 end
