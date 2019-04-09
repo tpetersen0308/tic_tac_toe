2b80d351fa984cda874e46103ec93311854bcb7c
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-09 09:46:12 -0700

    add remaining combinations to @win_combos

--- lib/game.ex
+++ lib/game.ex
@@ -1,6 +1,15 @@
 defmodule Game do
-  @win_combos [{1,2,3}, {4,5,6}]
-  
+  @win_combos [
+    {1, 2, 3}, 
+    {4, 5, 6},
+    {7, 8, 9},
+    {1, 4, 7},
+    {2, 5, 8},
+    {3, 6, 9},
+    {1, 5, 9},
+    {3, 5, 7}
+    ]
+
   def turn_count(board) do
     Enum.filter(Map.values(board), &(&1)) |> Enum.count
   end
