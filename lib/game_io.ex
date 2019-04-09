c18b2eb8dd2f30304e1473e832ace81386453307
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-09 14:25:17 -0700

    refactor to print the position numbers in unoccupied positions

--- lib/game_io.ex
+++ lib/game_io.ex
@@ -1,6 +1,6 @@
 defmodule GameIO do
   
   def print_board(board) do
-    IO.puts(" #{board[1]} | #{board[2]} | #{board[3]} \n-----------\n #{board[4]} | #{board[5]} | #{board[6]} \n-----------\n #{board[7]} | #{board[8]} | #{board[9]} ")
+    IO.puts(" #{board[1] || 1} | #{board[2] || 2} | #{board[3] || 3} \n-----------\n #{board[4] || 4} | #{board[5] || 5} | #{board[6] || 6} \n-----------\n #{board[7] || 7} | #{board[8] || 8} | #{board[9] || 9} ")
   end
 end
