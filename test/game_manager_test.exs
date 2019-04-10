1cd33f305c001c664cccc9a221b5bbc011c909d0
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-10 11:22:03 -0700

    add test for GameManager.turn prompting the user with a properly interpolated message

--- test/game_manager_test.exs
+++ test/game_manager_test.exs
@@ -9,6 +9,12 @@ defmodule GameManagerTest do
       board = Board.empty()
       turn(board)
       assert called GameIO.get_input("X")
-    end 
+    end
+
+    test_with_mock "it prompts the user for input with a properly interpolated message", GameIO, [get_input: fn(_) -> nil end] do
+      board = Board.empty() |> Board.update(5, "X")
+      turn(board)
+      assert called GameIO.get_input("O")
+    end
   end
 end 
