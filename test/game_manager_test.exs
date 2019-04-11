09dc2f0ace1ff45b2ea67962deb4a990adefc536
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-11 09:33:07 -0700

    add integration test for if turn can update the board

--- test/game_manager_test.exs
+++ test/game_manager_test.exs
@@ -14,6 +14,7 @@ defmodule GameManagerTest do
           get_input: fn(current_player) -> 
             IO.puts("It is #{current_player}'s turn. Please select from the available positions.")
           end,
+          parse_input: fn(_) -> nil end,
         ]
       }
     ]) do
@@ -21,6 +22,23 @@ defmodule GameManagerTest do
       assert capture_io(fn -> 
         turn(board)
       end) == "It is X's turn. Please select from the available positions.\n"
+    end
+  end
+
+  test "it can update the board with the player's move" do
+    with_mocks([
+      {
+        GameIO,
+        [],
+        [
+          get_input: fn(_) -> nil end,
+          parse_input: fn(_) -> 5 end,
+        ]
+      },
+    ]) do
+      board = %{ 1 => "X", 2 => nil, 3 => "X", 4 => "O", 5 => nil, 6 => nil, 7 => "O", 8 => "X", 9 => "O"}
+      updated_board = %{ 1 => "X", 2 => nil, 3 => "X", 4 => "O", 5 => "X", 6 => nil, 7 => "O", 8 => "X", 9 => "O"}
+      assert updated_board == turn(board)
     end
   end
 end 
