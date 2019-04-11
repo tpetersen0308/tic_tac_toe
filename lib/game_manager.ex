3af5d39951048978e911dbdf2d6064fe275db31a
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-11 10:53:10 -0700

    refactor to display the board

--- lib/game_manager.ex
+++ lib/game_manager.ex
@@ -1,5 +1,6 @@
 defmodule GameManager do
   def turn(board) do
+    GameIO.print_board(board)
     current_player = Game.current_player(board)
     user_input = GameIO.get_input(current_player)
     user_move = GameIO.parse_input(user_input)
