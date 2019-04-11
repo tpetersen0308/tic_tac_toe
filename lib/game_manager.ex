39b69b44b7f225ad4e96e2c8f2b535bda53bb603
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-11 09:26:41 -0700

    add turn function that prompts the current player for input

--- lib/game_manager.ex
+++ lib/game_manager.ex
@@ -1,6 +1,6 @@
 defmodule GameManager do
   def turn(board) do
     current_player = Game.current_player(board)
-    GameIO.get_input(current_player)
+    user_input = GameIO.get_input(current_player)
   end
 end
