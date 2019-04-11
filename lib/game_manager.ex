20c3739c8fe72da95f9bcafe060b468a857d25a4
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-11 09:33:45 -0700

    refactor turn to update the board

--- lib/game_manager.ex
+++ lib/game_manager.ex
@@ -2,5 +2,8 @@ defmodule GameManager do
   def turn(board) do
     current_player = Game.current_player(board)
     user_input = GameIO.get_input(current_player)
+    user_move = GameIO.parse_input(user_input)
+
+    Board.update(board, user_move, current_player)
   end
 end
