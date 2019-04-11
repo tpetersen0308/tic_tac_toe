475a8680d40d9dcd0239289446b10843e68e293a
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-11 15:06:25 -0700

    extract input fetching cycle to get_move function

--- lib/game_manager.ex
+++ lib/game_manager.ex
@@ -1,21 +1,24 @@
 defmodule GameManager do
   def turn(board) do
     GameIO.print_board(board)
+    user_move = get_move(board)
+    valid_move = validate_input(board, board[user_move], user_move)
+
+    Board.update(board, valid_move, Game.current_player(board))
+  end
+
+  def get_move(board) do
     current_player = Game.current_player(board)
     user_input = GameIO.get_input(current_player)
     user_move = GameIO.parse_input(user_input)
-
-    valid_move = validate_input(board, board[user_move], user_move)
-    Board.update(board, valid_move, current_player)
   end
 
   defguard is_valid_move(board, target_cell, move) when not is_integer(move) or move not in 1..map_size(board) or target_cell != nil
 
   def validate_input(board, target_cell, move) when is_valid_move(board, target_cell, move) do
     IO.puts("You entered an invalid move.")
-    current_player = Game.current_player(board)
-    user_input = GameIO.get_input(current_player)
-    user_move = GameIO.parse_input(user_input)
+    user_move = get_move(board)
+    
     validate_input(board, board[user_move], user_move)
   end
 
