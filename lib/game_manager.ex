5032ec66d6b2ce0bb4a66c6b7117b03df286a8da
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-11 14:39:20 -0700

    add function clause for base case of validating user input (input is valid)

--- lib/game_manager.ex
+++ lib/game_manager.ex
@@ -10,5 +10,13 @@ defmodule GameManager do
 
   def validate_input(board, target_cell, move) when not is_integer(move) or move not in 1..map_size(board) or target_cell != nil do
     IO.puts("Invalid input.")
+    current_player = Game.current_player(board)
+    user_input = GameIO.get_input(current_player)
+    user_move = GameIO.parse_input(user_input)
+    validate_input(board, board[user_move], user_move)
+  end
+
+  def validate_input(board, target_cell, move) do
+    move
   end
 end
