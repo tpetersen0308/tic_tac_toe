27287704f70aa8976abcb94cc310beebfcdd551a
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-17 15:56:01 -0700

    add executable

---
+++ lib/cli.ex
@@ -0,0 +1,5 @@
+defmodule CLI do
+  def main(args \\ []) do
+    GameManager.start
+  end
+end
--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule TicTacToe.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
+      escript: [main_module: CLI],
       deps: deps()
     ]
   end
