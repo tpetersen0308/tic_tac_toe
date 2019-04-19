ada8522748a4c07b42aea4407f5648662e49887b
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-19 11:34:58 -0700

    rename get_random_move to turn

--- lib/computer_player.ex
+++ lib/computer_player.ex
@@ -1,5 +1,5 @@
 defmodule ComputerPlayer do
-  def get_random_move(board_manager, board) do
+  def turn(board_manager, board) do
       board |> board_manager.available_positions |> Enum.random
   end
 end
