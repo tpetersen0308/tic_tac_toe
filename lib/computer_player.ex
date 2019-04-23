92b93be34b569eab809f32c14fea094a81845eb9
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-23 12:12:29 -0700

    fix spacing

--- lib/computer_player.ex
+++ lib/computer_player.ex
@@ -1,5 +1,5 @@
 defmodule TicTacToe.ComputerPlayer do
   def move(board_manager, board) do
-      board |> board_manager.available_positions |> Enum.random
+    board |> board_manager.available_positions |> Enum.random
   end
 end
