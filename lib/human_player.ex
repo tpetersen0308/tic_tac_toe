26ee7c9a468b9d58cf2af5efe63ef6b7930ac21a
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-23 11:56:43 -0700

    refactor to call more abstract user interface functions

--- lib/human_player.ex
+++ lib/human_player.ex
@@ -3,13 +3,13 @@ defmodule TicTacToe.HumanPlayer do
   def move(deps, board, player, valid_move \\ false)
 
   def move(deps, board, player, valid_move) when not valid_move do
-    { move, is_valid, msg } = deps.user_interface.get_move(player.token)
+    { move, is_valid, invalid_input_msg } = deps.user_interface.get_move(player.token)
       |> deps.validator.validate_move(board)
 
     valid_move = cond do
       not is_valid -> 
         deps.user_interface.print_board(board)
-        deps.user_interface.invalid_input(move, msg)
+        deps.user_interface.message(invalid_input_msg)
         is_valid
       true -> move
     end
--- test/human_player_test.exs
+++ test/human_player_test.exs
@@ -6,7 +6,8 @@ defmodule TicTacToe.HumanPlayerTest do
   defmodule FakeIO do
     def get_move(_), do: Helpers.Stack.pop()
     def print_board(_), do: nil
-    def invalid_input(_,_), do: nil
+    def message(_), do: nil
+    # def invalid_input(_,_), do: nil
   end
 
   defmodule FakeValidator do
