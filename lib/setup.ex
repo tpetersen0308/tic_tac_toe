a4e6c0b65bff3052da1108b828061afac8637bd7
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-24 11:17:42 -0700

    refactor game setup function to adapt to new validation return value and better leverage pattern matching for recursion

--- lib/setup.ex
+++ lib/setup.ex
@@ -2,26 +2,15 @@ defmodule TicTacToe.Setup do
   @player1_token Application.get_env(:tic_tac_toe, :player1_token)
   @player2_token Application.get_env(:tic_tac_toe, :player2_token)
 
-  def game(deps, game_mode \\ nil, is_valid_game_mode \\ false)
-
-  def game(deps, game_mode, true) do
-    players_deps = %{
-      user_interface: deps.user_interface, 
-      validator: deps.validator
-    }
-    
-    {deps.board_manager.empty, players(players_deps, game_mode)}
-  end
-
-  def game(deps, _game_mode, _is_valid_game_mode) do
-    {user_interface, validator} = {deps.user_interface, deps.validator}
-
-    {game_mode, is_valid} = user_interface.get_input(:game_mode)
-      |> validator.validate_numeric_selection(1..2)
-    
-    if not is_valid, do: user_interface.message(:invalid_game_mode)
-
-    game(deps, game_mode, is_valid)
+  def game(deps) do
+    validation_result = deps.user_interface.get_input(:game_mode)
+      |> deps.validator.validate_numeric_selection(1..2)
+    with {:error, _game_mode_selection} <- validation_result do
+      deps.user_interface.message(:invalid_game_mode)
+      game(deps)
+    else {:ok, game_mode_selection} ->
+      {deps.board_manager.empty, players(%{user_interface: deps.user_interface, validator: deps.validator}, game_mode_selection)}
+    end
   end
 
   def players(deps, game_mode) do
