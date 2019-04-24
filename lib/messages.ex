81677906dc36a6334997d83e2df2af12c8e447a8
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-24 11:33:50 -0700

    implement abstracted player selector and mode options

--- lib/messages.ex
+++ lib/messages.ex
@@ -1,13 +1,17 @@
 defmodule TicTacToe.Messages do
   @quit_char Application.get_env(:tic_tac_toe, :quit_char)
+  @human_v_human_mode Application.get_env(:tic_tac_toe, :human_v_human_mode)
+  @human_v_computer_mode Application.get_env(:tic_tac_toe, :human_v_computer_mode)
+  @player1_selector Application.get_env(:tic_tac_toe, :player1_selector)
+  @player2_selector Application.get_env(:tic_tac_toe, :player2_selector)
 
   def get(term) do
     messages = %{
       welcome: "\nWelcome to Tic Tac Toe!",
       player1_turn: "\nIt is player one's turn. Please enter an available position: ",
       player2_turn: "\nIt is player two's turn. Please enter an available position: ",
-      game_mode: "\nEnter a number to select a game mode: \n  1 >> Human vs. Human\n  2 >> Human vs. Computer\n  ",
-      player: "\nEnter a number to select a player: \n  1 >> Player 1\n  2 >> Player 2\n  ",
+      game_mode: "\nEnter a number to select a game mode: \n  #{@human_v_human_mode} >> Human vs. Human\n  #{@human_v_computer_mode} >> Human vs. Computer\n  ",
+      player: "\nEnter a number to select a player: \n  #{@player1_selector} >> Player 1\n  #{@player2_selector} >> Player 2\n  ",
       invalid_game_mode: "\nThat is not a valid game mode. Please try again.",
       invalid_player: "\nThat is not a valid player option. Please try again.",
       nan: "\nThat is not an integer. Please try again.",
