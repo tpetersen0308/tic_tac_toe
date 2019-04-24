6e301161f611fa4491c3d6299ced90341f104fb9
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-24 11:08:41 -0700

    extract logic for returning human v human and human v computer players to separate methods

--- lib/setup.ex
+++ lib/setup.ex
@@ -24,40 +24,27 @@ defmodule TicTacToe.Setup do
     game(deps, game_mode, is_valid)
   end
 
-  # def players(deps, game_mode, selection \\ nil, is_valid_player \\ false)
-
-  # def players(_deps, _game_mode, selection, true) do
-  #   {%{token: @player1_token, human: selection == 1}, %{token: @player2_token, human: selection == 2}}
-  # end
-
-  # def players(deps, game_mode, _selection, _is_valid_player) do
-  #   {user_interface, validator} = {deps.user_interface, deps.validator}
-
-  #   cond do
-  #     game_mode == 1 -> {%{token: @player1_token, human: true}, %{token: @player2_token, human: true}}
-  #     true -> 
-  #       {selection, is_valid} = user_interface.get_input(:player)
-  #       |> validator.validate_numeric_selection(1..2)
-        
-  #       if not is_valid, do: user_interface.message(:invalid_player)
-  #       players(deps, game_mode, selection, is_valid)
-  #   end
-  # end
-
   def players(deps, game_mode) do
     with 1 <- game_mode do
-      {%{token: @player1_token, human: true}, %{token: @player2_token, human: true}}
-    else
-        2 -> 
-          validation_result = deps.user_interface.get_input(:player)
-            |> deps.validator.validate_numeric_selection(1..2)
-          with { :error, selection } <- validation_result do
-            deps.user_interface.message(:invalid_player)
-            players(deps, game_mode)
-          else 
-            { :ok, selection } -> 
-              {%{token: @player1_token, human: selection == 1}, %{token: @player2_token, human: selection == 2}}
-          end
+      human_v_human_players()
+    else 2 ->
+      human_v_computer_players(deps) 
     end
-  end 
+  end
+
+  def human_v_human_players() do
+    {%{token: @player1_token, human: true}, %{token: @player2_token, human: true}}
+  end
+
+  def human_v_computer_players(deps) do
+    validation_result = deps.user_interface.get_input(:player)
+      |> deps.validator.validate_numeric_selection(1..2)
+    with { :error, selection } <- validation_result do
+      deps.user_interface.message(:invalid_player)
+      human_v_computer_players(deps)
+    else 
+      { :ok, selection } -> 
+        {%{token: @player1_token, human: selection == 1}, %{token: @player2_token, human: selection == 2}}
+    end
+  end
 end
