a3d2933038de7dfc780f0bda5009a4287c7751fd
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-25 15:02:00 -0700

    extract move scoring logic from minimax into separate function

--- lib/computer_player.ex
+++ lib/computer_player.ex
@@ -13,28 +13,32 @@ defmodule TicTacToe.ComputerPlayer do
 
   def minimax(deps, board, current_player, max_player) do
     with true <- deps.game_status.is_over(deps.board_manager, board) do
-      cond do
-        deps.game_status.winner(board) == max_player -> %{score: 10}
-        deps.game_status.winner(board) == opponent(max_player) -> %{score: -10}
-        deps.game_status.winner(board) == nil -> %{score: 0}
-      end
+      score_move(deps.game_status.winner(board), max_player, opponent(max_player))
     else false ->
       available_positions = deps.board_manager.available_positions(board)
-      
+
       moves = Enum.map(available_positions, fn position -> 
         next_board = deps.board_manager.update(board, position, current_player)
         next_player = opponent(current_player)
         %{position: position, score: minimax(deps, next_board, next_player, max_player).score}
       end)
-      best_move = case current_player do
-        max_player -> Enum.reduce(moves, %{position: nil, score: -10}, fn (move, acc) -> if move.score >= acc.score, do: move, else: acc end)
+
+      cond do
+        _current_player = max_player -> Enum.reduce(moves, %{position: nil, score: -10}, fn (move, acc) -> if move.score >= acc.score, do: move, else: acc end)
         true -> Enum.reduce(moves, %{position: nil, score: 10}, fn (move, acc) -> if move.score <= acc.score, do: move, else: acc end)
       end
-      best_move
     end 
   end
 
-  def opponent(token) do
+  defp score_move(winner, max_player, min_player) do
+    cond do
+      winner == max_player -> %{score: 10}
+      winner == min_player -> %{score: -10}
+      winner == nil -> %{score: 0}
+    end    
+  end
+
+  defp opponent(token) do
     if token == @player1_token, do: @player2_token, else: @player1_token
   end
 end
