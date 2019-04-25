8f86f69fe27cdd466bbfdf2807b157c360f452cb
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-25 15:14:36 -0700

    extract logic for selecting best move from array of scored moves into separate function

--- lib/computer_player.ex
+++ lib/computer_player.ex
@@ -15,18 +15,14 @@ defmodule TicTacToe.ComputerPlayer do
     with true <- deps.game_status.is_over(deps.board_manager, board) do
       score_move(deps.game_status.winner(board), max_player, opponent(max_player))
     else false ->
-      available_positions = deps.board_manager.available_positions(board)
-
-      moves = Enum.map(available_positions, fn position -> 
+      scored_moves = deps.board_manager.available_positions(board)
+        |> Enum.map(fn position -> 
         next_board = deps.board_manager.update(board, position, current_player)
         next_player = opponent(current_player)
         %{position: position, score: minimax(deps, next_board, next_player, max_player).score}
       end)
 
-      cond do
-        _current_player = max_player -> Enum.reduce(moves, %{position: nil, score: -10}, fn (move, acc) -> if move.score >= acc.score, do: move, else: acc end)
-        true -> Enum.reduce(moves, %{position: nil, score: 10}, fn (move, acc) -> if move.score <= acc.score, do: move, else: acc end)
-      end
+      best_move(scored_moves, max_player, opponent(max_player), current_player)
     end 
   end
 
@@ -36,6 +32,13 @@ defmodule TicTacToe.ComputerPlayer do
       ^min_player -> %{score: -10}
       nil -> %{score: 0}
     end    
+  end
+
+  defp best_move(scored_moves, max_player, min_player, current_player) do
+    case current_player do
+      ^max_player -> Enum.reduce(scored_moves, %{position: nil, score: -10}, fn (move, acc) -> if move.score >= acc.score, do: move, else: acc end)
+      ^min_player -> Enum.reduce(scored_moves, %{position: nil, score: 10}, fn (move, acc) -> if move.score <= acc.score, do: move, else: acc end)
+    end
   end
 
   defp opponent(token) do
