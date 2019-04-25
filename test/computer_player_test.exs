974d3642d9195904a05240916d4824a92534ac4d
tpetersen0308 <travis.e.petersen@gmail.com>
2019-04-25 14:40:46 -0700

    add mocked components and fix erroneous expectations in tests

--- test/computer_player_test.exs
+++ test/computer_player_test.exs
@@ -6,19 +6,59 @@ defmodule TicTacToe.ComputerPlayerTest do
 
   @player1 %{token: "X"}
   @player2 %{token: "O"}
-  @fake_deps %{board_manager: FakeBoardManager}
 
   defmodule FakeBoardManager do
-    def available_positions(board) do
-      Map.keys(:maps.filter fn _, v -> !v end, board)
-    end
-    def turn_count(board) do
-      Enum.filter(Map.values(board), &(&1)) |> Enum.count
-    end
     def update(board, position, token) do
       Map.put(board, position, token)
     end
+
+    def is_full(board) do
+      Enum.all?(Map.values(board), &(&1))
+    end
+
+    def turn_count(board) do
+      Enum.filter(Map.values(board), &(&1)) |> Enum.count
+    end
+
+    def available_positions(board) do
+      Map.keys(:maps.filter fn _, v -> !v end, board)
+    end
   end
+
+  defmodule FakeGameStatus do
+    @win_combos [
+      {1, 2, 3}, 
+      {4, 5, 6},
+      {7, 8, 9},
+      {1, 4, 7},
+      {2, 5, 8},
+      {3, 6, 9},
+      {1, 5, 9},
+      {3, 5, 7}
+    ]
+
+    def check_win(board) do
+      @win_combos |> Enum.find(fn {pos1, pos2, pos3} ->
+        board[pos1] == board[pos2] and board[pos1] == board[pos3] and !!board[pos1]
+      end)
+    end
+
+    def winner(board) do
+      case check_win(board) do
+        {winner, _, _} -> board[winner]
+        nil -> nil
+      end
+    end
+
+    def check_draw(board_manager, board) do
+      board_manager.is_full(board) and !check_win(board)
+    end
+
+    def is_over(board_manager, board) do
+      !!check_win(board) or check_draw(board_manager, board)
+    end
+  end
+  @fake_deps %{board_manager: FakeBoardManager, game_status: FakeGameStatus}
 
   describe "get_best_move" do
     test "it can select the best available move from an empty board" do
@@ -30,7 +70,7 @@ defmodule TicTacToe.ComputerPlayerTest do
     test "it can select a winning move over a blocking move" do
       board = %{1 => nil, 2 => "O", 3 => "O", 4 => nil, 5 => "X", 6 => "X", 7 => "X", 8 => nil, 9 => "O" }
     
-      assert get_best_move(@fake_deps, board, @player1) == 2
+      assert get_best_move(@fake_deps, board, @player1) == 4
     end
 
     test "it can select a blocking move when no winning move is available" do
@@ -40,7 +80,7 @@ defmodule TicTacToe.ComputerPlayerTest do
     end
 
     test "it can select a winning move when many options are available" do
-      board = %{1 => nil, 2 => nil, 3 => "O", 4 => "O", 5 => "X", 6 => nil, 7 => nil, 8 => nil, 9 => "X" }
+      board = %{1 => "X", 2 => nil, 3 => "O", 4 => "O", 5 => "X", 6 => nil, 7 => nil, 8 => nil, 9 => nil }
     
       assert get_best_move(@fake_deps, board, @player1) == 9
     end
